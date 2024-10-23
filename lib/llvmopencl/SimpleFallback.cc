e350b3729067c224a7cc299cd7c0d136c91c1462
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2024-10-23 15:35:53 +0300

    added new workhandlertype fallback that can be activated with POCL_WORK_GROUP_METHOD=fallback

--- lib/llvmopencl/SimpleFallback.cc
+++ lib/llvmopencl/SimpleFallback.cc
@@ -1,5 +1,8 @@
 #include "LLVMUtils.h"
 #include "SimpleFallback.h"
+#include "WorkitemHandlerChooser.h"
+
+#include <iostream>
 
 #define PASS_NAME "simplefallback"
 #define PASS_CLASS pocl::SimpleFallback
@@ -22,7 +25,24 @@ private:
 
 
 llvm::PreservedAnalyses SimpleFallback::run(llvm::Function &F, llvm::FunctionAnalysisManager &AM) {
-  
+    
+    // We only want to process kernel functions
+    if (!isKernelToProcess(F)){
+        return llvm::PreservedAnalyses::all();
+    }
+    
+
+    WorkitemHandlerType WIH = AM.getResult<WorkitemHandlerChooser>(F).WIH;
+    if (WIH != WorkitemHandlerType::FALLBACK)
+    {
+        return llvm::PreservedAnalyses::all();
+    }
+    
+
+    if(WIH == WorkitemHandlerType::FALLBACK){
+        std::cout << "WIH  is of type FALLBACK" << std::endl;
+    }
+
     llvm::errs() << F.getName() << "\n";
     return llvm::PreservedAnalyses::all();
 
